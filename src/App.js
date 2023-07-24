fe3c979a0246f9a52d2da5652f4e35a5974d6fc2
mfkfawas <mohammed.fawas@mobcoder.com>
2023-07-24 09:46:37 +0300

    rendering the items list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false }
+];
+
 export default function App() {
-  <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
@@ -14,17 +21,38 @@ function Logo() {
 function Form() {
   return (
     <div className="add-form">
-      <h1>What do you need for your trip?</h1>
+      <h3>What do you need for your trip?</h3>
     </div>
   );
 }
 
 function PackingList() {
-  <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>You have x items on your list and you already packed X(x%)</footer>
+    <footer className="stats">
+      You have x items on your list and you already packed X(x%)
+    </footer>
   );
 }
